ada91dbba26d229a8db2e785dab26411a51ee274
dhanalakshmikonar <konardhanalakshmi@gmail.com>
2025-07-11 15:05:12 +0530

    files modified

--- public/script.js
+++ public/script.js
@@ -98,11 +98,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  const hamburger = document.getElementById('hamburger');
+const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('navLinks');
+const links = navLinks.querySelectorAll('a');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
+});
+
+// Hide the nav menu when a link is clicked
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
 });
 
 
